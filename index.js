3203f2cb94a04f1957ac315ae732d8b9cadb309a
hieu <hieuvnautotech@gmail.com>
2022-09-06 14:45:10 +0700

    up

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
-const bodyParse = require("body-parser");
-const expressHandlebars = require("express-handlebars");
+const cors = require("cors");
+const bodParser = require("body-parser");
+// const authRoutes = require("./routes/auth-routes");
+// const homeRoutes = require("./routes/home-routes");
 
-var app = express();
-app.listen(5000, () => {
-  console.log("Sever is listening on port 5000");
-});
+const app = express();
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(homeRoutes.routes);
+app.use(authRoutes.routes);
+
+app.listen(3000, () =>
+  console.log("App is listening on url http://localhost:3000")
+);
